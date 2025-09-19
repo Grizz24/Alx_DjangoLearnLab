2ff8eed79b450bb4c6f628246d777ef8b9d2faf5
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-09-20 00:00:50 +0200

    Add files via upload

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -10,4 +12,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [   
     
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), 
     path('books/', BookList.as_view(), name='book-list'),
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
 ]
