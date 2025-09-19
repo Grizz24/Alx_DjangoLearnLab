a60fdd6ad0f459546c7733a1daedfea3103f94ca
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-09-19 23:14:38 +0200

    Add files via upload

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import BookList
 
-urlpatterns = [
+urlpatterns = [   
+    
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')), 
     path('books/', BookList.as_view(), name='book-list'),
 ]
-
