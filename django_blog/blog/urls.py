577bd4aefe2b8b71929c745bd119690dce1b9566
Branden Ranoto <89339676+Grizz24@users.noreply.github.com>
2025-10-05 23:20:12 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -17,9 +17,10 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
-    path('posts/<int:post_id>/comments/new/', add_comment, name='comment-add'),
+    path('posts/<int:pk>/comments/new/', add_comment, name='comment-add'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 
 ]
+
 
